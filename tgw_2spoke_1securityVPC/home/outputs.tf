74f151cc8914d94be9ce47a53fd6b4edc4d7d1de
germanium-git@gmx.com <BzBKX35wL%AzFhP5b54BLd2$>
2019-12-06 08:41:58 +0100

    spoke and security vpc

--- tgw_2spoke_1securityVPC/home/outputs.tf
+++ tgw_2spoke_1securityVPC/home/outputs.tf
@@ -1,3 +1,4 @@
+# Display TGW id
 output "tgw_id" {
   value = module.security_vpc.tgw_id
 }
@@ -7,14 +8,23 @@ output "fortigate_public_ip" {
   value = module.firewall.fg_public_ip
 }
 
-
 # Display FortiGate VM management IP
 output "fortigate_management_ip" {
   value = module.firewall.fg_mng_ip
 }
 
-
 # Display instance Id to log in and reset password
 output "fortigate_instance_id" {
   value = module.firewall.fg_instance_id
 }
+
+# Display test VM in VPC-A IP address
+output "vm_vpc_a_ip_address" {
+  value = module.test_vm-a.ec2_public_ip
+}
+
+# Display test VM in VPC-B IP address
+output "vm_vpc_b_ip_address" {
+  value = module.test_vm-b.ec2_public_ip
+}
+
